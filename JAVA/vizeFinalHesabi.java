a3155beb0868c15ca1b5c2495a9a1a982666584b
Caglayan Sancaktar <caglayan45@users.noreply.github.com>
2019-10-07 20:03:10 +0300

    vizeFinalHesabi
    fonksiyonClassVizeFinalHesabi.java kodlarındaki kullanılan(çağırılan) class.

---
+++ JAVA/vizeFinalHesabi.java
@@ -0,0 +1,14 @@
+
+package metoddeneme;
+
+public class vizeFinalHesabi {
+    
+    
+    public static double hesapla(int vize,int finl){
+        
+        return (double)((vize*0.4)+(finl*0.6));
+    }
+    
+    
+    
+}
