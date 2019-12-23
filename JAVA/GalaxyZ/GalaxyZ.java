edf9240c97f627ad912a91163aa647f0d11fa6cc
Çağlayan <38992192+caglayan45@users.noreply.github.com>
2019-12-23 14:22:54 +0300

    GalaxyZMainSinifi

---
+++ JAVA/GalaxyZ/GalaxyZ.java
@@ -0,0 +1,24 @@
+package GalaxyZ;
+
+public class GalaxyZ {
+
+    public static void main(String[] args) {
+        AlfaRobot a1 = new AlfaRobot();
+        BetaRobot b1 = new BetaRobot();
+        GamaRobot g1 = new GamaRobot();
+
+        a1.BinaYap();
+        a1.BinaYap();
+
+        b1.AgacDik();
+
+        g1.Calis();
+        g1.Calis();
+        g1.Calis();
+
+        g1.Bilgi();
+        b1.Bilgi();
+        a1.Bilgi();
+    }
+
+}
